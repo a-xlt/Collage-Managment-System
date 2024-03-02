854566e7a674a1f2fd070dd95a00415def022e26
a-xlt <85548683+a-xlt@users.noreply.github.com>
2024-03-03 02:23:11 +0300

    .

--- Student/Delete.aspx.cs
+++ Student/Delete.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +14,50 @@ namespace Collage_Managment_System.Student
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
 
+        }
+
+        protected void DeleteBTN_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+            con.Open();
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            command.Connection = con;
+            command.CommandText = "delete FROM Student where id = " + StudentDDL.SelectedValue;
+            int x = command.ExecuteNonQuery();
+            if (x > 0)
+            {
+                errorLB.Text = "تم حذف الطالب ";
+                errorLB.Visible = true;
+                errorLB.CssClass = "alert alert-success h3";
+            }
+        }
+
+        protected void SearchBTN_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+            con.Open();
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            command.Connection = con;
+            command.CommandText = "SELECT Id , Name FROM Student Where Id= "+idSearch.Text;
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                ListItem listItem = new ListItem()
+                {
+                    Value = reader[0].ToString(),
+
+                    Text = reader[1].ToString()
+                };
+
+
+                StudentDDL.Items.Add(listItem);
+            }
         }
     }
 }
